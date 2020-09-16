69a7dcc15afefc2f62df5b39837d16f334194dd4
Steve <steven.lee@deltatre.com>
2020-09-16 16:23:21 -0700

    update packages and added tailwind ui package, keen-slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 // See https://tailwindcss.com/docs/configuration for details
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   purge: ["./src/**/*.js"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms")],
+  plugins: [require("@tailwindcss/ui"), require("@tailwindcss/custom-forms")],
 };
