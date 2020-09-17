291754f26f72ca5d330367d1888d1b1541e91ca7
Kevin Lee <kevin.ej.lee@gmail.com>
2020-09-17 03:17:35 -0700

    carousel disaster

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import TwoByTwo from "../components/two-by-two";
+import Carousel from "../components/better-carousel";
 
 function IndexPage() {
   return (
@@ -11,7 +12,11 @@ function IndexPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-    <TwoByTwo/>
+      <React.StrictMode>
+        <Carousel />
+      </React.StrictMode>
+
+      <TwoByTwo />
       {/* <section className="text-center">
         <div className="bg-gray-100">
           <div className="pt-12 sm:pt-16 lg:pt-20">
