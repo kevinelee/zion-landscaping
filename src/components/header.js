aad9c4525126464447235055cd4a751661532e7c
Kevin Lee <kevin.ej.lee@gmail.com>
2020-09-16 22:22:38 -0700

    header stuff.  working on changing color of button on click

--- src/components/header.js
+++ src/components/header.js
@@ -14,8 +14,8 @@ function Header() {
   `);
 
   return (
-    <header className="bg-gray-800">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
+    <header className="bg-gray-800 sticky relative">
+      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8 absolute sticky">
         <Link to="/">
           <h1 className="flex items-center text-white no-underline">
             <img
@@ -29,17 +29,9 @@ function Header() {
         </Link>
 
         <button
-          className="items-center block px-3 py-2 text-white border border-white rounded md:hidden"
+          className="items-center block px-3 py-2 text-white border border-black  rounded md:hidden"
           onClick={() => toggleExpansion(!isExpanded)}
         >
-          {/* <svg
-            className="w-3 h-3 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg> */}
           {isExpanded ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
--- src/pages/about.js
+++ src/pages/about.js
@@ -15,11 +15,7 @@ function AboutPage() {
       <section className="flex flex-col items-center md:flex-row">
         <div className="md:w-2/3 md:mr-8">
           <blockquote className="pl-4 font-serif leading-loose text-justify border-l-4 border-gray-900">
-            The point is... to live one&apos;s life in the full complexity of
-            what one is, which is something much darker, more contradictory,
-            more of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to the civilized being who glides on
-            the surface and fits smoothly into the world.
+
           </blockquote>
 
           <cite className="block mt-4 text-xs font-bold text-right uppercase">
@@ -34,5 +30,7 @@ function AboutPage() {
     </Layout>
   );
 }
+
+
 
 export default AboutPage;
