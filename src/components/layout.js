0bbfd21f9d19e885c2d4b7e36924ad4f64d5b439
Kevin Lee <kevin.ej.lee@gmail.com>
2020-09-18 02:30:08 -0700

    i think i fixed the slider portion

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,32 +12,6 @@ function Layout({ children }) {
         {children}
       </main>
 
-      {/* <footer className="bg-blue-700">
-        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
-          <p className="text-white">
-            Created by{` `}
-            <a
-              className="font-bold no-underline"
-              href="https://bryant.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Taylor Bryant
-            </a>
-          </p>
-
-          <p>
-            <a
-              className="font-bold text-white no-underline"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </p>
-        </nav>
-      </footer> */}
       <footer>
         <div className="bg-gray-200">
           <div className="max-w-screen-xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
