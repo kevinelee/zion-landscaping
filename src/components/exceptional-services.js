0cf4af7f494aeb8fc088283c3515239fe5814947
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-21 03:55:46 -0700

    added some backgorund color to start off

--- src/components/exceptional-services.js
+++ src/components/exceptional-services.js
@@ -2,70 +2,72 @@ import React from "react";
 
 export default function ExceptionalService() {
   return (
-    <section className="flex flex-col lg:flex-row justify-center py-8 mx-auto max-w-screen-xl">
-      <div className="lg:max-w-sm rounded overflow-hidden mx-2 border-4 border-gray-200 my-3 lg:mb-0">
-        <div className="px-6 py-4 text-3xl">
-          <h1 className="border-b">
-            Exceptional
-            <br />
-            <span className="text-green-300">Services We Offer</span>
-          </h1>
+    <section className=" bg-gray-50">
+      <div className="max-w-screen-xl flex flex-col lg:flex-row justify-center py-8 mx-auto">
+        <div className="lg:max-w-sm rounded overflow-hidden mx-2 border-4 border-gray-200 my-3 lg:mb-0">
+          <div className="px-6 py-4 text-3xl">
+            <h1 className="border-b">
+              Exceptional
+              <br />
+              <span className="text-green-300">Services We Offer</span>
+            </h1>
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Lawn & Garden Care</div>
+            <p className="text-gray-700 text-base">
+              We provide exceptional landscaping services to a wide range of
+              commercial and residential properties for over 35 years, including
+              large corporate environments, city parks, shopping malls and
+              appartments. Our experienced landscapers set the standard each day
+              in landscape design, paving, hardscaping. We will whip your yard
+              into shape in no time.
+            </p>
+          </div>
         </div>
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Lawn & Garden Care</div>
-          <p className="text-gray-700 text-base">
-            We provide exceptional landscaping services to a wide range of
-            commercial and residential properties for over 35 years, including
-            large corporate environments, city parks, shopping malls and
-            appartments. Our experienced landscapers set the standard each day
-            in landscape design, paving, hardscaping. We will whip your yard
-            into shape in no time.
-          </p>
+        <div className="lg:max-w-sm rounded overflow-hidden mx-2 my-3 lg:mb-0">
+          <img
+            className="w-full"
+            src="https://demos.qreativethemes.com/thelandscaper/wp-content/uploads/sites/2/2015/07/trimming_hedge.jpg"
+            alt="Sunset in the mountains"
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Lawn & Garden Care</div>
+            <p className="text-gray-700 text-base">
+              We understand that your property is a big investment and we want
+              our customers to know that we are there for them to help them
+              accomplish the dreams that they may have for there property....
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="lg:max-w-sm rounded overflow-hidden mx-2 my-3 lg:mb-0">
-        <img
-          className="w-full"
-          src="https://demos.qreativethemes.com/thelandscaper/wp-content/uploads/sites/2/2015/07/trimming_hedge.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Lawn & Garden Care</div>
-          <p className="text-gray-700 text-base">
-            We understand that your property is a big investment and we want our
-            customers to know that we are there for them to help them accomplish
-            the dreams that they may have for there property....
-          </p>
+        <div className="lg:max-w-sm rounded overflow-hidden mx-2 my-3 lg:mb-0">
+          <img
+            className="w-full"
+            src="https://demos.qreativethemes.com/thelandscaper/wp-content/uploads/sites/2/2015/07/tree_removal.jpg"
+            alt="Sunset in the mountains"
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Planting & Removal</div>
+            <p className="text-gray-700 text-base">
+              While tree removal is the choice of last resort, it is sometimes
+              most necessary. Sick trees, trees that uproot sidewalks, cause
+              cracks in the foundation, disrupt sewer lines, have sustained...
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="lg:max-w-sm rounded overflow-hidden mx-2 my-3 lg:mb-0">
-        <img
-          className="w-full"
-          src="https://demos.qreativethemes.com/thelandscaper/wp-content/uploads/sites/2/2015/07/tree_removal.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Planting & Removal</div>
-          <p className="text-gray-700 text-base">
-            While tree removal is the choice of last resort, it is sometimes
-            most necessary. Sick trees, trees that uproot sidewalks, cause
-            cracks in the foundation, disrupt sewer lines, have sustained...
-          </p>
-        </div>
-      </div>
-      <div className="lg:max-w-sm rounded overflow-hidden mx-2 my-3 lg:mb-0">
-        <img
-          className="w-full"
-          src="https://demos.qreativethemes.com/thelandscaper/wp-content/uploads/sites/2/2015/07/irrigation.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Irrigation & Drainage</div>
-          <p className="text-gray-700 text-base">
-            Watering your lawn and is the key to preserving its lushness and
-            beauty. Whether you are looking for a brand new irrigation system to
-            maintain your lawn or the replacement of a sprinkler...
-          </p>
+        <div className="lg:max-w-sm rounded overflow-hidden mx-2 my-3 lg:mb-0">
+          <img
+            className="w-full"
+            src="https://demos.qreativethemes.com/thelandscaper/wp-content/uploads/sites/2/2015/07/irrigation.jpg"
+            alt="Sunset in the mountains"
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Irrigation & Drainage</div>
+            <p className="text-gray-700 text-base">
+              Watering your lawn and is the key to preserving its lushness and
+              beauty. Whether you are looking for a brand new irrigation system
+              to maintain your lawn or the replacement of a sprinkler...
+            </p>
+          </div>
         </div>
       </div>
     </section>
