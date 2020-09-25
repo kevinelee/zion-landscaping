309c93557128fb7c839dda407bcee880de194745
Kevin Lee <kevin.ej.lee@gmail.com>
2020-09-24 22:36:59 -0700

    added some dummy info

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 export default function Testimonials(){
+
+  const testimonials = [
+    {
+      name: "Jimothy Butler",
+      comments:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non dui dolor. Aliquam feugiat ipsum eu lorem pharetra, nec euismod odio fringilla. Vestibulum ornare efficitur.",
+    },
+    {
+      name: "Kevin Booker",
+      comments:
+        "Quisque nunc libero, tempus sit amet ullamcorper porttitor, malesuada eget justo. Pellentesque tincidunt quam leo, ac ultricies dolor imperdiet eu. Etiam consequat bibendum augue, vitae.",
+    },
+    {
+      name: "Lebron Jimmy",
+      comments:
+        "Aenean commodo euismod tellus, id vehicula arcu vestibulum vitae. Cras tempor tortor purus, in tincidunt justo molestie sed. Phasellus vel blandit lectus. Sed quis massa.",
+    },
+  ];
   return(
-    <div>bing</div>
+    <section></section>
   )
 }
