92f78c20d3fccbfbd712868a70d36e2330c7f7a7
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-28 04:27:07 -0700

    landscaper code abstracted

--- src/components/about-us-page/landscapers/index.js
+++ src/components/about-us-page/landscapers/index.js
@@ -46,69 +46,6 @@ export default function Landscapers() {
                 <Landscaper key={ls.firstName} src={ls.image} alt={ls.firstName} name={`${ls.firstName} ${ls.lastName}`} bio={ls.bio}/>
               )
             })}
-            {/* <Landscaper src={jimmy} name="Jimmy Butler" alt="Jimmy Butler"/> */}
-            {/* <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div className="flex-shrink-0">
-                <img className="h-48 w-full object-cover" src={Jimmy} alt="" />
-              </div>
-              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div className="flex-1">
-                  <a href="#" className="block">
-                    <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
-                      Boost your conversion rate
-                    </h3>
-                    <p className="mt-3 text-base leading-6 text-gray-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Architecto accusantium praesentium eius, ut atque fuga
-                      culpa, similique sequi cum eos quis dolorum.
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div className="flex-shrink-0">
-                <img className="h-48 w-full object-cover" src={Devin} alt="" />
-              </div>
-              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div className="flex-1">
-                  <a href="#" className="block">
-                    <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
-                      How to use search engine optimization to drive sales
-                    </h3>
-                    <p className="mt-3 text-base leading-6 text-gray-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Velit facilis asperiores porro quaerat doloribus, eveniet
-                      dolore. Adipisci tempora aut inventore optio animi.,
-                      tempore temporibus quo laudantium.
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div className="flex-shrink-0">
-                <img className="h-48 w-full object-cover" src={Lebron} alt="" />
-              </div>
-              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div className="flex-1">
-                  <a href="#" className="block">
-                    <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
-                      Improve your customer experience
-                    </h3>
-                    <p className="mt-3 text-base leading-6 text-gray-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Sint harum rerum voluptatem quo recusandae magni placeat
-                      saepe molestiae, sed excepturi cumque corporis perferendis
-                      hic.
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div> */}
-
 
           </div>
         </div>
