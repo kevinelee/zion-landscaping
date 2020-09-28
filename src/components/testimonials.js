8bfb644a5de702824e3057516820402276dc78b3
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-28 03:45:15 -0700

    testimonial looks funky, but it's working

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -9,7 +9,7 @@ function Testimonials() {
       name: "Jimmy Butler",
       comments:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non dui dolor. Aliquam feugiat ipsum eu lorem pharetra, nec euismod odio fringilla. Vestibulum ornare efficitur.",
-      image: Jimmy
+      image: Jimmy,
     },
     {
       name: "Devin Booker",
@@ -21,7 +21,7 @@ function Testimonials() {
       name: "Lebron James",
       comments:
         "Aenean commodo euismod tellus, id vehicula arcu vestibulum vitae. Cras tempor tortor purus, in tincidunt justo molestie sed. Phasellus vel blandit lectus. Sed quis massa.",
-        image: Lebron,
+      image: Lebron,
     },
   ];
 
@@ -32,18 +32,22 @@ function Testimonials() {
 
   return (
     <section className="bg-gray-50">
-      <div className="flex justify-center max-w-7xl h-96 mx-auto py-8">
-        <div className="w-1/4 flex flex-col ">
+      <div className="flex flex-col lg:flex-row justify-center max-w-7xl h-72 lg:h-96 mx-auto px-2 lg:py-8 mb-4 items-center">
+        <div className="flex lg:flex-col lg:w-1/4 justify-center">
           {testimonials && testimonials.length > 0
             ? testimonials.map((testimonial) => {
                 return (
                   <div
                     key={testimonial.name}
                     onClick={() => setTestimonial(testimonial)}
-                    className="h-32 flex justify-center items-center border-2 m-2 hover:border-gray-400 rounded-md"
+                    className="h-16 flex justify-center items-center border-2 m-2 hover:border-gray-400 rounded-md px-4"
                   >
-                    <img src={testimonial.image} alt={testimonial.name} className="h-12 w-12 object-cover rounded-full"/>
-                    {testimonial.name}
+                    <img
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                      className="h-12 w-12 object-cover rounded-full"
+                    />
+                    <span className="hidden md:block">{testimonial.name}</span>
                   </div>
                 );
               })
@@ -52,8 +56,9 @@ function Testimonials() {
 
         {/* comment section */}
         {testimonial.comments ? (
-          <div className="w-1/2 bg-gray-200 flex justify-center items-center rounded-md shadow-lg">
-            <p className="p-8">{testimonial.comments}</p>
+          <div className="md:w-4/5 lg:w-1/2 bg-gray-200 flex justify-center items-center rounded-md shadow-lg flex-col py-4 px-8 lg:h-56">
+            <h1 className="md:hidden text-2xl ">{testimonial.name}</h1>
+            <p className="border-t-2 border-black md:border-none">{testimonial.comments}</p>
           </div>
         ) : null}
       </div>
