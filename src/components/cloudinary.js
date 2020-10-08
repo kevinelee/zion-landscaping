a2be1d8ce5962459f4bce055d423ed48b1dd2b69
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-10-08 13:42:14 -0700

    trying to figure out how to get this from class to function

--- src/components/cloudinary.js
+++ src/components/cloudinary.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 // import Img from "gatsby-image";
 import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
 import "regenerator-runtime/runtime";
-// import LazyLoad from "react-lazyload";
+// import { lazyload } from "react-lazyload";
+
+// @lazyload({
+//   height: 200,
+//   once: true,
+//   offset: 100,
+// })
 
 export default class GridGallery extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      gallery: [],
+      link: [],
       selectedButton: "patio",
       isPortal: false,
       imageId: "",
@@ -34,7 +40,7 @@ export default class GridGallery extends Component {
         `https://res.cloudinary.com/stevelee/image/list/${selector}.json`
       );
       const gallery = await res.json();
-      this.setState({ gallery: gallery.resources });
+      this.setState({ link: gallery.resources });
     } catch (error) {
       console.log(error);
     }
@@ -50,91 +56,93 @@ export default class GridGallery extends Component {
 
   render() {
     return (
+      
       <section className="max-w-7xl flex justify-center mx-auto">
-      <CloudinaryContext
-        cloudName="stevelee"
-      >
-        <div className="button-group flex justify-center">
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("patio")}
-          >
-            Patio
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("front-yard")}
-          >
-            Front Yard
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("driveway")}
-          >
-            Driveway
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("fire-place")}
-          >
-            Fire Place
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("patio-cover")}
-          >
-            Patio Cover
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("putting-green")}
-          >
-            Putting Green
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("barbeque")}
-          >
-            Barbeque
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("fire-place")}
-          >
-            Fire Place
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("pool-deck")}
-          >
-            Pool Deck
-          </button>
-          <button
-            className="services-button m-2"
-            onClick={this.handleSelect("fountain")}
-          >
-            Fountain
-          </button>
-        </div>
-        <div className="grid-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-          {this.state.gallery.length > 0 &&
-            this.state.gallery.map((data) => {
-              return (
-                <div className="grid-gallery__image mx-auto m-2 lg:m-4 gap-1 cursor-pointer" key={data.public_id}>
-                  <Image publicId={data.public_id}>
-                    <Transformation
-                      crop="scale"
-                      width="300"
-                      height="200"
-                      dpr="auto"
-                      responsive_placeholder="blank"
-                    />
-                  </Image>
-                </div>
-              );
-            })}
-        </div>
-      </CloudinaryContext>
+        <CloudinaryContext cloudName="stevelee">
+          <div className="button-group flex justify-center">
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("patio")}
+            >
+              Patio
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("front-yard")}
+            >
+              Front Yard
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("driveway")}
+            >
+              Driveway
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("fire-place")}
+            >
+              Fire Place
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("patio-cover")}
+            >
+              Patio Cover
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("putting-green")}
+            >
+              Putting Green
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("barbeque")}
+            >
+              Barbeque
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("fire-place")}
+            >
+              Fire Place
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("pool-deck")}
+            >
+              Pool Deck
+            </button>
+            <button
+              className="services-button m-2"
+              onClick={this.handleSelect("fountain")}
+            >
+              Fountain
+            </button>
+          </div>
+          <div className="grid-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+            {this.state.gallery.length > 0 &&
+              this.state.gallery.map((data) => {
+                return (
+                  <div
+                    className="grid-gallery__image mx-auto m-2 lg:m-4 gap-1 cursor-pointer"
+                    key={data.public_id}
+                  >
+                    <Image publicId={data.public_id}>
+                      <Transformation
+                        crop="scale"
+                        width="300"
+                        height="200"
+                        dpr="auto"
+                        responsive_placeholder="blank"
+                      />
+                    </Image>
+                  </div>
+                );
+              })}
+          </div>
+        </CloudinaryContext>
       </section>
     );
   }
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import GridGallery from "../components/cloudinary"
+// import GridGallery from "../components/cloudinary"
+import GridGallery from "../components/test-cloudinary"
 
 function ServicesPage() {
   return (
