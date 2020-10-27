13e8631690daa93d7a204d84b92b6a872b71d6d1
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-10-26 23:47:22 -0700

    testimonials adjusted with svg

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Jimmy from "../images/jimmy butler.png";
 import Devin from "../images/devin booker.png";
 import Lebron from "../images/lebron james.png";
+import QuotationSVG from "./Svg/QuotationSVG";
 
 function Testimonials() {
   const testimonials = [
@@ -29,7 +30,7 @@ function Testimonials() {
 
   return (
     <section className="bg-gray-50">
-      <h3 className="max-w-screen-xl mx-auto text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 lg:py-8 lg:px-6">
+      <h3 className="max-w-screen-xl mx-auto text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 lg:py-8 px-4 md:px-6">
         Testimonials
       </h3>
       <div className="flex flex-col lg:flex-row justify-center max-w-7xl  mx-auto px-2 lg:py-8 mb-4 items-center">
@@ -61,14 +62,8 @@ function Testimonials() {
               {testimonial.name}
             </h1>
             <div>
-              <p className="border-t-2 border-black md:border-none font-semibold text-md">
-                <svg
-                  className="absolute z-10 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600 opacity-25 "
-                  fill="currentColor"
-                  viewBox="0 0 32 32"
-                >
-                  <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                </svg>
+              <p className="border-t-2 border-black md:border-none font-semibold text-md lg:w-9/12 mx-auto py-4">
+                <QuotationSVG />
                 {testimonial.comments}
               </p>
             </div>
