9d84fef14b24a5667290573dc42c79e68696f8d5
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-10-26 23:36:01 -0700

    completed quotation

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -27,10 +27,9 @@ function Testimonials() {
 
   const [testimonial, setTestimonial] = useState(testimonials[0]);
 
-
   return (
     <section className="bg-gray-50">
-      <h3 className="max-w-screen-xl mx-auto text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 lg:py-8">
+      <h3 className="max-w-screen-xl mx-auto text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 lg:py-8 lg:px-6">
         Testimonials
       </h3>
       <div className="flex flex-col lg:flex-row justify-center max-w-7xl  mx-auto px-2 lg:py-8 mb-4 items-center">
@@ -58,10 +57,21 @@ function Testimonials() {
         {/* comment section */}
         {testimonial.comments ? (
           <div className="bg-gray-200 flex justify-center items-center rounded-md lg:w-3/4 shadow-lg flex-col py-4 px-8 lg:h-56">
-            <h1 className="md:hidden text-2xl font-semibold">{testimonial.name}</h1>
-            <p className="border-t-2 border-black md:border-none font-semibold text-md">
-              {testimonial.comments}
-            </p>
+            <h1 className="md:hidden text-2xl font-semibold">
+              {testimonial.name}
+            </h1>
+            <div>
+              <p className="border-t-2 border-black md:border-none font-semibold text-md">
+                <svg
+                  className="absolute z-10 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600 opacity-25 "
+                  fill="currentColor"
+                  viewBox="0 0 32 32"
+                >
+                  <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                </svg>
+                {testimonial.comments}
+              </p>
+            </div>
           </div>
         ) : null}
       </div>
